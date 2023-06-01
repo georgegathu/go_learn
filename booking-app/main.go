bcf647e62c3ee852797ff725adf3e6baf0c6793e
georgegathu <ggeorge.gathu@gmail.com>
2023-06-01 11:06:53 +0300

    My Go booking app update

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,9 +7,17 @@ func main() {
 	const confrenceTickets = 69
 	var remainingTickets = 69
 
-	fmt.Println("Welcome to our ", confrenceName," booking app")
-	fmt.Println("We have a total of", confrenceTickets, "tickets and", remainingTickets, "are still available!")
+	fmt.Printf("Welcome to our %v booking app\n", confrenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available!\n", confrenceTickets, remainingTickets)
 	fmt.Println("Get your ticket here to attend")
 	
+
+	var userName string // Ask users for their name
+	var userTickets int // Displays how many tickets the user bought
+
+	userName = "Allan"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
