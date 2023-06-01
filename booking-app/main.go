1d31105cafe3648360a8e99eb9e48f40863098a9
georgegathu <ggeorge.gathu@gmail.com>
2023-06-01 10:42:12 +0300

    My Go booking app

---
+++ booking-app/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var confrenceName = "Go confrence"
+	const confrenceTickets = 69
+	var remainingTickets = 69
+
+	fmt.Println("Welcome to our ", confrenceName," booking app")
+	fmt.Println("We have a total of", confrenceTickets, "tickets and", remainingTickets, "are still available!")
+	fmt.Println("Get your ticket here to attend")
+	
+
+}
