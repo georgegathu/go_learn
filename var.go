05c8ada2182115e1eaeebdb2b28bd7f0c8bc60d5
georgegathu <ggeorge.gathu@gmail.com>
2023-06-02 23:59:22 +0300

    Variables in Go

---
+++ var.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    // declare a variable of type string
+    var name string = "Gathu"
+
+    // declare a variable of type integer
+    var age int = 25
+
+    // declare a variable of type float
+    var height float64 = 1.75
+
+    // print the values of the variables
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Height:", height)
+}
