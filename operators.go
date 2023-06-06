9c256e129394f776a19fa5f18445a3f3ae9462a6
georgegathu <ggeorge.gathu@gmail.com>
2023-06-06 23:41:23 +0300

    Operators in Go

--- operators.go
+++ operators.go
@@ -1,17 +1,15 @@
+// Operators in GO
+
 package main
 
 import "fmt"
 
 func main () {
-	var a int = 44
-	var b float32 = 4.44
-	const pi float64 = 3.14151
+	x,y := 44,45
 
-	x,y := 14,15
-
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(pi)
-	fmt.Println(x,",",y)
-	
+	fmt.Println("x + y = ", x+y)
+	fmt.Println("x - y = ", x-y)
+	fmt.Println("x * y = ", x*y)
+	fmt.Println("x / y = ", x/y)
+	fmt.Println("x mod y = ", x%y)
 }
