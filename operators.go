77616e6b6a6eef3f1b60c8fc79d087445940e381
georgegathu <ggeorge.gathu@gmail.com>
2023-06-25 23:15:05 +0300

    Boolean operators

--- operators.go
+++ operators.go
@@ -12,4 +12,11 @@ func main () {
 	fmt.Println("x * y = ", x*y)
 	fmt.Println("x / y = ", x/y)
 	fmt.Println("x mod y = ", x%y)
+
+	isbool := true
+	hate := false
+
+	fmt.Println(isbool && hate)
+	fmt.Println(isbool || hate)
+	fmt.Println(isbool)
 }
